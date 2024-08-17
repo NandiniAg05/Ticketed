37ad7bd8222745552a9943ac8200bc3c52eccfa4
Nandini Agarwal <nandiniagarwal0509@gmail.com>
2024-08-17 20:37:02 +0530

    update database

---
+++ prisma/migrations/20240712144653_ticketed_final/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `qrCode` on the `Ticket` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Ticket" DROP COLUMN "qrCode";
