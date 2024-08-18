dd7330e1c6b60e6903378a1a4c1112eef542e8ff
Nandini Agarwal <nandiniagarwal0509@gmail.com>
2024-08-18 16:22:13 +0530

    poster

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          { hostname: 'img.clerk.com' },
-          { hostname: 'firebasestorage.googleapis.com' },
-        ],
-      },
-}
+  images: {
+    remotePatterns: [
+      { hostname: 'img.clerk.com' },
+      { hostname: 'firebasestorage.googleapis.com' },
+      { hostname: 'ticketed.netlify.app' }, // Ensure this is a separate pattern
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
